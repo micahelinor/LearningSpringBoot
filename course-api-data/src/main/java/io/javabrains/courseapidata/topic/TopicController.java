7299acbd6d68710637617f5b01d00f6e820d35b6
micahelinor <mferas@berkeley.edu>
2020-12-28 14:47:45 -0800

    adding spring jpa

---
+++ course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicController.java
@@ -0,0 +1,40 @@
+package springbootquickstart.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired  //the variable topicservice has a dependency
+    private TopicService topicservice;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicservice.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}") //{x} means that x is a variable
+    public Topic getTopic(@PathVariable String id) {    //@PathVariable  is indicating that id is variable that will come from the path
+        return topicservice.getTopic(id);
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic) {    //Request payload would have a json representation of the topic
+        topicservice.addTopic(topic);                   //then Spring MVC converts it from json to object "Topic"
+    }
+
+    @RequestMapping(method= RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicservice.updateTopic(topic, id);
+    }
+
+    @RequestMapping(method= RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicservice.deleteTopic(id);
+    }
+
+
+}
---
+++ course-api-data/src/main/java/io/javabrains/courseapidata/topic/TopicService.java
@@ -0,0 +1,47 @@
+package springbootquickstart.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
+public class TopicService{
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("Spring", "Spring", "Spring Framework"),
+            new Topic("Java", "Java", "Programming Language"),
+            new Topic("Maven", "Maven", "Dependency Manager")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        //Makes the topic list into stream
+        //Then filter any topic who has that id
+        //chooses the first  topic that has the id
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for(int i = 0; i <topics.size(); i++) {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t-> t.getId().equals(id));
+    }
+}
